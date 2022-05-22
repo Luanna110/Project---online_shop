240347d36f01769827cf9b489a26cee0b64e821b
Tomasz <tomasz1577@gmail.com>
2022-05-22 14:13:10 +0200

    Added catalog models, and building catalog views

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
     class Meta:
         ordering = ('name',)
@@ -26,8 +30,13 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
+
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+
     def __str__(self):
         return self.name
