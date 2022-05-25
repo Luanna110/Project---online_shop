f118d4228418590932760cbecb5d0af098652bbd
Tomasz <tomasz1577@gmail.com>
2022-05-25 19:45:12 +0200

    Shopping cart views

--- cart/cart.py
+++ cart/cart.py
@@ -9,7 +9,7 @@ class Cart(object):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session(settings.CART_SESSION_ID)
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
