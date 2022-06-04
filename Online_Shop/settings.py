4122b05bd789eca107dd33872afaa2b82ba8f3c4
Anna <anna.chlusewicz@gmail.com>
2022-06-04 11:12:43 +0200

    settings.py

--- Online_Shop/settings.py
+++ Online_Shop/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'orders.apps.OrdersConfig',
     'users',
-    'payment.apps.PaymentConfig',
+    'payment.apps.PaymentConfig',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
